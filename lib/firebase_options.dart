66ae25816e7833bfc87556e108a2ee95188d28d8
Amr Shipl <amrshipl09@gmail.com>
2026-03-30 13:35:34 +0000

    Fix: Add missing class structure and imports to firebase_options

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,3 +1,16 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    throw UnsupportedError(
+      'DefaultFirebaseOptions are not supported for this platform.',
+    );
+  }
+
   static const FirebaseOptions web = FirebaseOptions(
     apiKey: 'AIzaSyAR_ilDZbo9hPR0JJ2Wtal555N8VVWvqEQ',
     appId: '1:549455573441:web:a198bb1adb5f3b35c4ff40',
@@ -8,4 +21,5 @@
     storageBucket: 'aksab-erp.firebasestorage.app',
     measurementId: 'G-7GGD70EHNC',
   );
+}
 
