13e7d3aeca1596a32da1815d5eb381c06bda1066
Amr Shipl <amrshipl09@gmail.com>
2026-03-30 13:30:43 +0000

    Update: Migrate Admin Panel to Plaza (aksab-erp) Firebase

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,18 +1,11 @@
-import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb;
-
-class DefaultFirebaseOptions {
-  static FirebaseOptions get currentPlatform {
-    return web;
-  }
-
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyAA2JbmtD52JMCz483glEV8eX1ZDeK0fZE',
-    authDomain: 'aksabeg-b6571.firebaseapp.com',
-    projectId: 'aksabeg-b6571',
-    storageBucket: 'aksabeg-b6571.appspot.com',
-    messagingSenderId: '32660558108',
-    appId: '1:32660558108:web:102632793b65058953ead9',
+    apiKey: 'AIzaSyAR_ilDZbo9hPR0JJ2Wtal555N8VVWvqEQ',
+    appId: '1:549455573441:web:a198bb1adb5f3b35c4ff40',
+    messagingSenderId: '549455573441',
+    projectId: 'aksab-erp',
+    authDomain: 'aksab-erp.firebaseapp.com',
+    databaseURL: 'https://aksab-erp-default-rtdb.firebaseio.com',
+    storageBucket: 'aksab-erp.firebasestorage.app',
+    measurementId: 'G-7GGD70EHNC',
   );
-}
 
